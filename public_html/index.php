8e4e77cb88dc1f12ffb196016e5c878fec03071f
J.Haemin <io@jhaemin.com>
2018-04-21 22:11:44 +0900

    Create index.php

---
+++ public_html/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Povium - Post your vision.</title>
+	<link rel="stylesheet" type="text/css" href="css/home.css" />
+</head>
+<body>
+	<main id="povium-main">
+		<h1 class="title">Povium</h1>
+		<p class="manifesto">Publish your dream.</p>
+	</main>
+</body>
