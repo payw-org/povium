a42af39b7e4725e7e71286c5203e5269ea0ddae9
jhaemin <io@jhaemin.com>
2018-04-23 17:38:46 +0900

    updated home

--- public_html/global-inclusion/global-head-link.php
+++ public_html/global-inclusion/global-head-link.php
@@ -1,2 +1,3 @@
+<link rel="stylesheet" href="/global-inclusion/css/fonts.css" />
 <link rel="stylesheet" href="/global-inclusion/css/globalstyle.css" />
 <link rel="stylesheet" href="/global-inclusion/globalnav/css/globalnav.css" />
--- public_html/index.php
+++ public_html/index.php
@@ -4,6 +4,7 @@
 	<title>Povium - Post your vision.</title>
 	<link rel="stylesheet" type="text/css" href="css/home.css" />
 	<?php include_once $_SERVER['DOCUMENT_ROOT'].'/global-inclusion/global-head-link.php'; ?>
+	<?php include_once $_SERVER['DOCUMENT_ROOT'].'/global-inclusion/global-head-script.php'; ?>
 </head>
 <body>
 	<?php include_once $_SERVER['DOCUMENT_ROOT'].'/global-inclusion/globalnav/globalnav.php'; ?>
@@ -12,4 +13,5 @@
 			<input class="home-search-input" type="text" name="" value="" />
 		</div>
 	</main>
+	<script src="js/home.js"></script>
 </body>
