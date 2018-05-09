d00df01205429003e85567fb3ddf7e42dc5f21a9
H. chihoon <1000jaman@naver.com>
2018-05-09 13:36:00 +0900

    revert

--- public_html/global-inclusion/globalnav/js/globalnav.js
+++ public_html/global-inclusion/globalnav/js/globalnav.js
@@ -4,26 +4,26 @@ $(document).ready(function($) {
 
     // 네비게이션 검색 입력창 확장
     function expandSearchInput() {
-        $('#globalnav').addClass('search-active');
+        $('#gn-search-ui').addClass('active')
         $('#gn-search-input').focus();
     }
 
     function foldSearchInput() {
-        $('#globalnav').removeClass('search-active');
+        $('#gn-search-ui').removeClass('active')
     }
 
     $('#gn-search-ui .magnifier').on('click', function () {
-        if ($(this).hasClass('search-active')) {
-            // 검색
+        if ($(this).hasClass('active')) {
+            // 검색 시작
         } else {
-            expandSearchInput();
+            expandSearchInput()
         }
     })
 
     $(window).on('click', function (e) {
-        var clickedTarget = e.target;
+        var clickedTarget = e.target
         if (!clickedTarget.classList.contains('magnifier') && clickedTarget.id !== 'gn-search-input') {
-            foldSearchInput();
+            foldSearchInput()
         }
     })
 
