96b505b2b43de1950aea0b09d3a6b9bcf48dae3a
J.Haemin <io@jhaemin.com>
2018-05-09 15:23:04 +0900

    Initialize

--- public_html/global-inclusion/globalnav/globalnav.php
+++ public_html/global-inclusion/globalnav/globalnav.php
@@ -4,14 +4,14 @@
             <div id="gn-search">
     			<div id="gn-search-input-wrapper">
                     <div id="gn-search-ui">
-                        <div class="magnifier"></div>
         				<input id="gn-search-input" type="text" name="" value="" placeholder="검색" autocomplete="off">
+                        <div class="magnifier"></div>
                     </div>
     			</div>
-      		</div>
-              <div id="gn-logo-wrapper">
-                  <a id="gn-home-link" href="/"></a>
-              </div>
-          </div>
+    		</div>
+            <div id="gn-logo-wrapper">
+                <a id="gn-home-link" href="/"></a>
+            </div>
+        </div>
     </div>
 </nav>
--- public_html/global-inclusion/globalnav/js/globalnav.js
+++ public_html/global-inclusion/globalnav/js/globalnav.js
@@ -4,26 +4,26 @@ $(document).ready(function($) {
 
     // 네비게이션 검색 입력창 확장
     function expandSearchInput() {
-        $('#gn-search-ui').addClass('active')
+        $('#globalnav').addClass('search-active');
         $('#gn-search-input').focus();
     }
 
     function foldSearchInput() {
-        $('#gn-search-ui').removeClass('active')
+        $('#globalnav').removeClass('search-active');
     }
 
     $('#gn-search-ui .magnifier').on('click', function () {
-        if ($(this).hasClass('active')) {
-            // 검색 시작
+        if ($(this).hasClass('search-active')) {
+            // 검색
         } else {
-            expandSearchInput()
+            expandSearchInput();
         }
     })
 
     $(window).on('click', function (e) {
-        var clickedTarget = e.target
+        var clickedTarget = e.target;
         if (!clickedTarget.classList.contains('magnifier') && clickedTarget.id !== 'gn-search-input') {
-            foldSearchInput()
+            foldSearchInput();
         }
     })
 
