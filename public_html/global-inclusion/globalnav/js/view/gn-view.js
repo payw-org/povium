2c6c2b4f0cdce14a4ece5c0b26d47df54d2eea09
J.Haemin <io@jhaemin.com>
2018-05-12 16:20:47 +0900

    New global navigation view module.

---
+++ public_html/global-inclusion/globalnav/js/view/gn-view.js
@@ -0,0 +1,76 @@
+jQuery(document).ready(function($) {
+
+	$(document).on('click', function(e) {
+		if (!document.querySelector('#gn-search').contains(e.target)) {
+			globalNav.foldSearchInput();
+		}
+	})
+
+    var globalNav = new Vue({
+
+        el: '#globalnav',
+
+        data: {
+
+            searchResults: [
+				{
+                    "keyword": "Apple",
+                    "link": "https://www.apple.com/"
+                },
+                {
+                    "keyword": "Google",
+                    "link": "https://www.google.com/"
+                },
+                {
+                    "keyword": "Microsoft",
+                    "link": "https://www.microsoft.com/"
+                }
+            ],
+
+			searchKeyword: "",
+
+			classFlags: {
+				isSearchActive: false,
+				isSearchResultActive: false
+			}
+
+        },
+
+        methods: {
+
+            expandSearchInput: function() {
+				this.classFlags.isSearchActive = true;
+				this.$el.querySelector('#gn-search-input').focus();
+            },
+
+			foldSearchInput: function() {
+				this.classFlags.isSearchActive = false;
+				this.classFlags.isSearchResultActive = false;
+			},
+
+			handleSearchInput: function(e) {
+				if (e.which === 27) {
+					this.foldSearchInput();
+					return;
+				}
+
+				if (this.searchKeyword === "") {
+					this.classFlags.isSearchResultActive = false;
+				} else {
+					this.classFlags.isSearchResultActive = true;
+				}
+			},
+
+			handleMagnifierClick: function() {
+				if ($(this.$el).hasClass('search-active')) {
+		            // 검색
+		            console.warn('검색 시작');
+		        } else {
+		            this.expandSearchInput();
+		        }
+			}
+
+        }
+    })
+
+});
