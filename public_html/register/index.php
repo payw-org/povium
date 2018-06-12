c04dc2befb371102402aae1a4f73d64401c7086b
J.Haemin <io@jhaemin.com>
2018-06-13 00:30:50 +0900

    Prepare for sign up page.

--- public_html/register/index.php
+++ public_html/register/index.php
@@ -5,12 +5,20 @@
 		<title>회원가입</title>
 		<?php include_once $_SERVER['DOCUMENT_ROOT'] . "/global-inclusion/global-meta.php"; ?>
 		<?php include_once $_SERVER['DOCUMENT_ROOT'] . "/global-inclusion/global-css.php"; ?>
+		<link rel="stylesheet" href="css/register.css">
 	</head>
 	<body>
 		<?php include_once $_SERVER['DOCUMENT_ROOT'] . "/global-inclusion/globalnav/globalnav.php"; ?>
 
 		<main id="register-main">
-			<h1>글 쓰는 사람을 위한 플랫폼.</h1>
+			<div class="introduce-header">
+				<h1>좋은 글, 세상을 바꾸는 힘.</h1>
+				<p>지금 참여하고 수익도 얻으세요.</p>
+			</div>
+			<div class="provider">
+				<input type="text" name="" value="" placeholder="아이디">
+				<input type="text" name="" value="" placeholder="패스워드">
+			</div>
 		</main>
 
 		<?php include_once $_SERVER['DOCUMENT_ROOT'] . "/global-inclusion/global-script.php"; ?>
