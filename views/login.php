568a2ffceb6327ffa8cc13e60b131619df697758
J.Haemin <io@jhaemin.com>
2018-07-17 14:57:11 +0900

    Fixed page title

--- views/login.php
+++ views/login.php
@@ -3,7 +3,7 @@ use Povium\Base\TemplateEngine;
 
 $te = new TemplateEngine();
 $te->render("/resources/templates/base.phtml", [
-	"title" => "Povium | 페이지를 찾을 수 없음",
+	"title" => "Povium | 로그인",
 	"main" => "/resources/templates/login-main.phtml",
 	"script" => '<script src="/build/js/login.built.js"></script>'
 ]);
