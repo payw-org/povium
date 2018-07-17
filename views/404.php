b90e3bc04664df57ac4f0520c7269554128f66e5
fairyhooni <1000jaman@naver.com>
2018-07-17 11:58:26 +0900

    Update

--- views/404.php
+++ views/404.php
@@ -3,7 +3,7 @@ use Povium\Base\TemplateEngine;
 
 $te = new TemplateEngine();
 $te->render("/resources/templates/base.phtml", [
-	"title" => "Povium | 로그인",
+	"title" => "Povium | 페이지를 찾을 수 없음",
 	"main" => "/resources/templates/404.phtml",
 	"script" => '<script src="/build/js/login.built.js"></script>'
 ]);
