cd169babe0a87788d148b8c665a244ac61b8232a
fairyhooni <1000jaman@naver.com>
2018-07-20 11:28:12 +0900

    Minor update

--- config/connection.php
+++ config/connection.php
@@ -13,9 +13,9 @@ return [
 	'username' => 'root',
 	'password' => 'pro1000j',
 	'opt' => [
-		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-		PDO::ATTR_EMULATE_PREPARES => false
+		\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+		\PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+		\PDO::ATTR_EMULATE_PREPARES => false
 	]
 ];
 
