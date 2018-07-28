bc925728b84daffac20cab137bfefb8c2c943d58
J.Haemin <io@jhaemin.com>
2018-07-28 17:31:57 +0900

    Add

---
+++ resources/assets/js/logout.js
@@ -0,0 +1,29 @@
+import AJAX from "./AJAX";
+
+if (document.querySelector("#globalnav .sign-out")) {
+
+	let signOutButton = document.querySelector("#globalnav .sign-out");
+
+	signOutButton.addEventListener("click", function(e) {
+
+		e.preventDefault();
+
+		let ajax = new AJAX();
+		ajax.chirp({
+			type: "post",
+			url: "/logout",
+			success: function(response) {
+
+				try {
+					let result = JSON.parse(response);
+					window.location.replace(result['redirect']);
+				} catch(e) {
+					alert(e + " " + response);
+				}
+
+			}
+		});
+		
+	});
+
+}
