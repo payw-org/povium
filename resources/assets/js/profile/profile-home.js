3dda9de32cf7ca49753cfce8c68b51fc05e5d76a
J.Haemin <jhaemin@JHaemins-MacBook-Pro.local>
2018-09-09 15:37:57 +0900

    Updated profile home

--- resources/assets/js/profile/profile-home.js
+++ resources/assets/js/profile/profile-home.js
@@ -13,17 +13,26 @@ window.addEventListener('scroll', function() {
 		offset = 0
 	}
 
-	let scrollTop = document.documentElement.scrollTop
+	let scrollTop = document.documentElement.scrollTop || document.body.scrollTop
 
 	let historyElmDistance = scrollTop + historyElm.getBoundingClientRect().top - 60
 
-	let afterTransform = (historyElmDistance - offset) / 1.8
+	let afterTransform = (historyElmDistance - offset) / 2
 	let afterOpacity = (offset) / (historyElmDistance)
 
-	profileInfo.style.transform = 'translateY(' + afterTransform + 'px) scale(' + (offset + 720) / (historyElmDistance + 720) + ')'
+	profileInfo.style.transform = 'translate(0, ' + afterTransform + 'px) scale(' + (offset + 720) / (historyElmDistance + 720) + ')'
 	profileInfo.style.opacity = afterOpacity
 	profileInfo.style.filter = 'blur(' + afterTransform / 6 + 'px)'
 
 	
 
+})
+
+document.querySelectorAll('.menu-item').forEach(function(menuItem) {
+	menuItem.addEventListener('click', function() {
+		document.querySelector('.menu-item.selected').classList.remove('selected')
+		this.classList.add('selected')
+		document.querySelector('.board-container.selected').classList.remove('selected')
+		document.querySelector('.board-' + this.getAttribute('data-mi-name')).classList.add('selected')
+	})
 })
