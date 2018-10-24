3c76f8f621a60b8ff25ff4d2cbb2c883cc0858a4
J.Haemin <jhaemin@JHaemins-MacBook-Pro.local>
2018-10-24 16:55:16 +0900

    New pvmNode

--- resources/assets/js/newEditor/PVMNode.js
+++ resources/assets/js/newEditor/PVMNode.js
@@ -1,17 +1,105 @@
-export default class PVMNode
-{
+import AT from "./config/AvailableTypes"
 
-	constructor()
-	{
+export default class PVMNode {
 
+	constructor() {
+
+		/**
+		 * @type {number}
+		 */
 		this.id = null
+		/**
+		 * @type {string}
+		 */
 		this.type = null
+		/**
+		 * @type {string}
+		 */
 		this.parentType = null
+		/**
+		 * @type {PVMNode}
+		 */
 		this.nextSibling = null
+		/**
+		 * @type {PVMNode}
+		 */
 		this.previousSibling = null
-		this.textHTML = null
-		this.rawText = null
+		// /**
+		//  * @type {string}
+		//  */
+		// this.textHTML = ""
+		// /**
+		//  * @type {string}
+		//  */
+		// this.rawText = ""
+		/**
+		 * @type {boolean}
+		 */
+		this.isConnected = false
+		/**
+		 * @type {Element}
+		 */
+		this.element = null
+		/**
+		 * @type {Element}
+		 */
+		this.textElement = null
 
+	}
+
+	setInnerHTML(html) {
+		if (html === "") {
+			html = "<br>"
+		}
+
+		this.textElement.innerHTML = html
+	}
+
+	fixEmptiness() {
+		if (this.textElement.textContent === "") {
+			this.textElement.innerHTML = "<br>"
+		}
+	}
+
+	/**
+	 *
+	 * @param {PVMNode} node
+	 */
+	isSameAs(node) {
+		return this.id === node.id
+	}
+
+	/**
+	 * @param {Element} elm
+	 */
+	replaceElement(elm) {
+		this.element = elm
+		if (AT.textContained.includes(this.type)) {
+			this.textElement = elm
+		}
+	}
+
+	generateDOM() {
+
+		let dom
+
+		if (AT.textContained.includes(this.type)) {
+			dom = document.createElement(this.type)
+			if (this.rawText === "") {
+				dom.innerHTML = "<br>"
+			} else {
+				dom.innerHTML = this.textHTML
+			}
+		}
+
+		dom.setAttribute("data-ni", this.id)
+
+		return dom
+
+	}
+
+	getTextContent() {
+		return this.textElement.textContent
 	}
 
 }
