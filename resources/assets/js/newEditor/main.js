f188953eb43e681703113b9d4baf40acfa357038
J.Haemin <jhaemin@JHaemins-MacBook-Pro.local>
2018-11-12 16:26:13 +0900

    try catch test

--- resources/assets/js/newEditor/main.js
+++ resources/assets/js/newEditor/main.js
@@ -1,3 +1,9 @@
 import PVMEditor from "./PVMEditor";
 
 let pvme = new PVMEditor(document.querySelector("#post-editor"))
+
+try {
+	console.log(JSON.parse(""))
+} catch (error) {
+	console.log(error)
+}
