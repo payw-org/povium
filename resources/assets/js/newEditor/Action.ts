1360ee258bdb410770db0022fc9f34dc618358c6
J.Haemin <io@jhaemin.com>
2018-11-13 19:27:43 +0900

    Formed an interface for undo actions.

--- resources/assets/js/newEditor/Action.ts
+++ resources/assets/js/newEditor/Action.ts
@@ -2,8 +2,19 @@ import PVMRange from "./PVMRange"
 import PVMNode from "./PVMNode"
 
 export interface Action {
-	targetNode?: PVMNode
-	targetNodes?: PVMNode[]
-	previousRange: PVMRange
-	nextRange: PVMRange
+	type               : string
+	targetNode?        : PVMNode
+	targetNodes?       : PVMNode[]
+	nextNode?          : PVMNode
+	previousRange      : PVMRange
+	nextRange          : PVMRange
+	finalAction?       : boolean
+	previousHTML?      : string
+	nextHTML?          : string
+	debris?            : string
+	key?               : string
+	previousType?      : string
+	previousParentType?: string
+	nextType?          : string
+	nextParentType?    : string
 }
