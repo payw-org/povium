2527521727685e1c86794c4aefea30964ac5e1c9
J.Haemin <io@jhaemin.com>
2018-11-14 14:05:54 +0900

    detect-browser

--- resources/assets/js/newEditor/main.ts
+++ resources/assets/js/newEditor/main.ts
@@ -1,3 +1,9 @@
 import PVMEditor from "./PVMEditor";
 
+const {detect} = require('detect-browser')
+const browser = detect()
+if (browser) {
+	console.log(browser.name)
+}
+
 let pvme = new PVMEditor(document.querySelector("#post-editor"))
