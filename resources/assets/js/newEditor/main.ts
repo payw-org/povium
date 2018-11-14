a5fb3d2b6ea8a2c95a023ea2beaf5391804b07c0
J.Haemin <jhaemin@JHaemins-MacBook-Pro.local>
2018-11-15 07:38:05 +0900

    Fixed

--- resources/assets/js/newEditor/main.ts
+++ resources/assets/js/newEditor/main.ts
@@ -6,7 +6,7 @@ import { ErrorManager } from "./ErrorManager"
 const browser = detect()
 if (browser.name === 'firefox') {
 	console.warn('You need FireFox with version at least 69.')
+	ErrorManager.showError('버전 69 이상의 파이어폭스가 팔요합니다.')
 }
 
 let pvme = new PVMEditor(document.querySelector("#post-editor"))
-ErrorManager.showError('버전 69 이상의 파이어폭스가 팔요합니다.')
