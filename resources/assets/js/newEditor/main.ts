fcbe3c5233ae296dbc8cbeaa219efe18f9447c71
J.Haemin <jhaemin@JHaemins-MacBook-Pro.local>
2018-11-15 07:36:54 +0900

    Ban firefox

--- resources/assets/js/newEditor/main.ts
+++ resources/assets/js/newEditor/main.ts
@@ -1,9 +1,12 @@
 import PVMEditor from "./PVMEditor";
+import {detect} from 'detect-browser'
+import Axios from 'axios'
+import { ErrorManager } from "./ErrorManager"
 
-const {detect} = require('detect-browser')
 const browser = detect()
-if (browser) {
-	console.log(browser.name)
+if (browser.name === 'firefox') {
+	console.warn('You need FireFox with version at least 69.')
 }
 
 let pvme = new PVMEditor(document.querySelector("#post-editor"))
+ErrorManager.showError('버전 69 이상의 파이어폭스가 팔요합니다.')
