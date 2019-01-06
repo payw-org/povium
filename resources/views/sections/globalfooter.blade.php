b9f0174e63559e767e3267038df31e791d15a9f4
J.Haemin <jhaemin@JHaemins-MacBook-Pro.local>
2019-01-06 16:14:39 +0900

    Updated copyright message

--- resources/views/sections/globalfooter.blade.php
+++ resources/views/sections/globalfooter.blade.php
@@ -1,4 +1,4 @@
 <footer id="globalfooter">
     <img src="/assets/images/povium/Povium-logo-shaded.svg" alt="">
-    <span class="copyright">Created by Povium</span>
+    <span class="copyright">Created by Povium &copy; 2018-2019</span>
 </footer>
