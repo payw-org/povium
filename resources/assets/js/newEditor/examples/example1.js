b3763a4585f3b269f43b77a9f836a08c2ab1a0a3
J.Haemin <io@jhaemin.com>
2019-01-09 23:18:43 +0900

    Updated an example based on RDM post format 1.0

--- resources/assets/js/newEditor/examples/example1.js
+++ resources/assets/js/newEditor/examples/example1.js
@@ -4,76 +4,58 @@ export const Example = {
 	body: "",
 	contents: [
 		{
-			type: "p",
-			data: [
-				{
-					type: "text",
-					data:
-						'"Blockchain" this, "blockchain" that. It\'s a concept so momentous that it has even managed to shed its article. '
-				},
-				{
-					type: "text",
-					data: "Proponents don’t speak of “the” blockchain or “a” blockchain.",
-					style: "bold"
-				},
-				{
-					type: "text",
-					data:
-						" Instead, they reverently preach Blockchain: the solution to all enterprise needs ("
-				},
-				{
-					type: "text",
-					data: "in particular, supply chain management",
-					style: "italic"
-				},
-				{
-					type: "text",
-					data: ")."
-				},
-				{
-					type: "text",
-					data: " first mering bold part first ",
-					style: "bold"
-				}
-			]
-		},
-		{
 			type: "image",
 			size: "",
 			url: "/assets/images/post-test-img-4.jpg",
-			caption: null
+			captionEnabled: false
 		},
 		{
 			type: "p",
 			data: [
 				{
-					type: "text",
-					data: "second mering bold part second ",
-					style: "bold"
+					"type": "styledText",
+					"data": [
+						{
+							"type": "styledText",
+							"data": [
+								{
+									"type": "rawText",
+									"data": "second <u>bold</u>merging bold part second "
+								}
+							],
+							"style": "italic"
+						}
+					],
+					"style": "bold"
 				},
+			]
+		},
+		{
+			type: "p",
+			data: [
 				{
-					type: "text",
-					data:
-						'"Blockchain" this, "blockchain" that. It\'s a concept so momentous that it has even managed to shed its article. '
-				},
-				{
-					type: "text",
-					data: "Proponents don’t speak of “the” blockchain or “a” blockchain.",
-					style: "bold"
-				},
-				{
-					type: "text",
-					data:
-						" Instead, they reverently preach Blockchain: the solution to all enterprise needs ("
-				},
-				{
-					type: "text",
-					data: "in particular, supply chain management",
-					style: "italic"
-				},
-				{
-					type: "text",
-					data: ")."
+					"type": "styledText",
+					"data": [
+						{
+							"type": "rawText",
+							"data": "second "
+						},
+						{
+							"type": "styledText",
+							"data": [
+								{
+									"type": "rawText",
+									"data": "merging"
+								}
+							],
+							"style": "italic"
+						},
+						{
+							"type": "rawText",
+							"data": " bold part second "
+						}
+					],
+					"style": "bold"
 				}
 			]
 		},
@@ -81,34 +63,25 @@ export const Example = {
 			type: "image",
 			size: "",
 			url: "/assets/images/post-test-img.gif",
+			captionEnabled: true,
 			caption: {
 				data: [
 					{
-						type: "text",
-						data: "This is an image caption."
+						"type": "rawText",
+						"data": "This is an image caption."
 					}
 				]
 			}
 		},
 		{
-			type: "p",
-			data: [
-				{
-					type: "text",
-					data: "A short sentence."
-				}
-			]
-		},
-		{
 			type: "blockquote",
 			data: [
 				{
-					type: "text",
-					data:
-						"The reason “blockchain” is such seductive marketing… is the subtle implication that the data structure alone — absent proof of work or open validation — could convey the same benefits as bitcoin."
+					type: "rawText",
+					data: "The reason “blockchain” is such seductive marketing… is the subtle implication that the data structure alone — absent proof of work or open validation — could convey the same benefits as bitcoin."
 				}
 			],
-			mode: "default"
+			kind: "bar"
 		}
 	],
 	isPremium: true
