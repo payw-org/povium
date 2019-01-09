4d67603bc0823440fb710b46799a921bad0631a7
J.Haemin <io@jhaemin.com>
2019-01-09 23:18:06 +0900

    New  features.

--- resources/assets/js/newEditor/dbg.ts
+++ resources/assets/js/newEditor/dbg.ts
@@ -1,12 +1,19 @@
 import SelectionManager from "./SelectionManager"
 import EditSession from "./EditSession";
+import Converter from "./Converter";
 
 export const dbg = function() {
 	let d = document.querySelector(".debug")
+	d.querySelector(".gcn").addEventListener("click", e => {
+		console.log(SelectionManager.getCurrentNode())
+	})
 	d.querySelector(".gcs").addEventListener("click", e => {
 		console.log(SelectionManager.getCurrentRange())
 	})
 	d.querySelector(".gcnl").addEventListener("click", e => {
 		console.log(EditSession.nodeList)
+	})
+	d.querySelector(".converter-stringify").addEventListener("click", e => {
+		console.log(Converter.stringify(EditSession.nodeList))
 	})
 }
