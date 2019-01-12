d4e9867ec52925d62f43509a68f3e7d4c60b1e98
fairyhooni <1000jaman@naver.com>
2019-01-13 00:19:07 +0900

    Created loader for sql.

---
+++ app/Loader/Database/Factory/SQLLoaderFactory.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This factory is responsible for creating "SQLLoader" instance.
+ *
+ * @author		H.Chihoon
+ * @copyright	2019 Povium
+ */
+
+namespace Readigm\Loader\Database\Factory;
+
+use Readigm\Base\Factory\AbstractChildFactory;
+
+class SQLLoaderFactory extends AbstractChildFactory
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function prepareArgs()
+	{
+	}
+}
---
+++ app/Loader/Database/SQLLoader.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Loader for sql.
+ *
+ * @author		H.Chihoon
+ * @copyright	2019 Povium
+ */
+
+namespace Readigm\Loader\Database;
+
+class SQLLoader
+{
+	/**
+	 * Load sql for creating table.
+	 *
+	 * @param	string	$table	Table name
+	 *
+	 * @return	string
+	 */
+	public function loadCreateSQL($table)
+	{
+		$file = require($_SERVER['DOCUMENT_ROOT'] . '/../database/create/' . $table . '_table.php');
+
+		$sql = $file['sql'];
+
+		return $sql;
+	}
+}
