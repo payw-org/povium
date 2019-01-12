7486c3766844cd882272142f66954e9fe5cd20b2
fairyhooni <1000jaman@naver.com>
2019-01-13 00:03:35 +0900

    Created new directory "Record".
    Created interface for table record manager.

---
+++ app/Record/RecordManagerInterface.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Interface for table record manager.
+ *
+ * @author		H.Chihoon
+ * @copyright	2019 Povium
+ */
+
+namespace Readigm\Record;
+
+use Readigm\Record\Exception\InvalidParameterNumberException;
+
+interface RecordManagerInterface
+{
+	/**
+	 * Returns a table record.
+	 *
+	 * @param	int|string	$id		Table record id
+	 *
+	 * @return array|false
+	 */
+	public function getRecord($id);
+
+	/**
+	 * Add new table record.
+	 *
+	 * @param mixed	Fields
+	 *
+	 * @return bool	Whether successfully added
+	 *
+	 * @throws InvalidParameterNumberException	If parameter number for creating record is not valid
+	 */
+	public function addRecord();
+
+	/**
+	 * Update some fields of specific table record.
+	 *
+	 * @param  int|string	$id		Table record id
+	 * @param  array 		$params	Assoc array (Field name => New value)
+	 *
+	 * @return bool	Whether successfully updated
+	 */
+	public function updateRecord($id, $params);
+
+	/**
+	 * Delete a table record.
+	 *
+	 * @param  int|string	$id		Table record id
+	 *
+	 * @return bool	Whether successfully deleted
+	 */
+	public function deleteRecord($id);
+}
