9588b591c5ee9a99343c8f9d623d298ef12be2eb
J.Haemin <io@jhaemin.com>
2019-01-14 15:30:07 +0900

    Added code tag.

--- resources/assets/js/newEditor/AvailableTypes.ts
+++ resources/assets/js/newEditor/AvailableTypes.ts
@@ -1,12 +1,12 @@
 export const AT = {
-	topTags: ["p", "h1", "h2", "h3", "blockquote", "li", "figure"],
-	textContainedTags: ["p", "h1", "h2", "h3", "blockquote", "li", "figcaption"],
+	topTags: ["p", "h1", "h2", "h3", "blockquote", "li", "figure", "code"],
+	textContainedTags: ["p", "h1", "h2", "h3", "blockquote", "li", "figcaption", "code"],
 	topTypes: ["p", "h1", "h2", "h3", "blockquote", "li", "image", "video"],
 	transformable: ["p", "h1", "h2", "h3", "blockquote", "li"],
 	mergeable: ["p", "h1", "h2", "h3", "li", "blockquote"],
 	splittable: ["p", "h1", "h2", "h3", "li", "blockquote"],
 	textOnly: ["p", "h1", "h2", "h3", "blockquote", "li"],
-	textContained: ["p", "h1", "h2", "h3", "blockquote", "li", "image"],
+	textContained: ["p", "h1", "h2", "h3", "blockquote", "li", "image", "code"],
 	alignable: ["p", "h1", "h2", "h3"],
 	textStyle: ["b", "strong", "em", "i", "strikethrough"],
 	headings: ["h1", "h2", "h3"]
