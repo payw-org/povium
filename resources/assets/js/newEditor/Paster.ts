4a2714e86e5df7e3ebffc00e81fb381d40e8e198
Jang Haemin <io@jhaemin.com>
2019-02-03 13:39:26 +0900

    Update

--- resources/assets/js/newEditor/Paster.ts
+++ resources/assets/js/newEditor/Paster.ts
@@ -1,248 +1,161 @@
 import TypeChecker from "./TypeChecker";
 import { AT } from "./AvailableTypes";
+import NodeManager from "./NodeManager";
+import EditSession from "./EditSession";
 
 export default class Paster {
-	complementP: HTMLElement
+	pasteArea: Element
 
 	constructor() {}
 
 	convert(elm: HTMLElement) {
+		this.pasteArea = elm
+
 		// this.loopChildren(elm, 1)
 		// this.loopChildren(elm, 2)
 		// this.loopChildren(elm, 3)
 		this.fixElement(elm, 1)
 		this.fixElement(elm, 2)
+		this.fixElement(elm, 3)
+		this.fixElement(elm, 4)
+
+		this.paste()
 	}
 
-	// fixElement(elm: HTMLElement, phase: number) {
-	// 	if (phase === 1) {
-	// 		let spanWalker = document.createTreeWalker(elm, NodeFilter.SHOW_ELEMENT, null)
+	fixElement(fixingTarget: HTMLElement, phase: number) {
+		if (phase === 1) {
+			let allElems = fixingTarget.querySelectorAll("*")
 
-	// 		let span
-	// 		let previousNode: Node
-	// 		while (spanWalker.nextNode()) {
-	// 			if (previousNode && previousNode.nodeName.toLowerCase() === "span") {
-	// 				let frag = document.createDocumentFragment()
-	// 				let n: Node
-	// 				while (n = previousNode.firstChild) {
-	// 					frag.appendChild(n)
-	// 				}
-	// 				previousNode.parentNode.replaceChild(frag, previousNode)
-	// 			}
-	// 			previousNode = spanWalker.currentNode
-	// 		}
-			
-	// 		// Final attack
-	// 		if (previousNode && previousNode.nodeName.toLowerCase() === "span") {
-	// 			let frag = document.createDocumentFragment()
-	// 			let n: Node
-	// 			while (n = previousNode.firstChild) {
-	// 				frag.appendChild(n)
-	// 			}
-	// 			previousNode.parentNode.replaceChild(frag, previousNode)
-	// 		}
+			for (let i = 0; i < allElems.length; i++) {
+				let elm = allElems[i]
+				if (!elm.parentElement) continue
 
-	// 		// Merge separated text nodes into a single text node
-	// 		elm.normalize()
-	// 	} else if (phase === 2) {
-	// 		let elms = document.createTreeWalker(elm, NodeFilter.SHOW_ELEMENT, null)
-	// 		let n: HTMLElement
-	// 		let previousNode: Node
-	// 		while (n = <HTMLElement> elms.nextNode()) {
-	// 			if (previousNode) {
-	// 				if (AT.availableTags.includes(previousNode.nodeName.toLowerCase())) {
-	// 					let pn = <HTMLElement> previousNode
-	// 					for (let i = 0; i < pn.attributes.length; i++) {
-	// 						pn.removeAttribute(pn.attributes[i].name)
-	// 					}
-	// 				} else if (previousNode.nodeName.toLowerCase() === "div") {
-	// 					let frag = document.createDocumentFragment()
-	// 					let fc
-	// 					while (fc = previousNode.firstChild) {
-	// 						frag.appendChild(fc)
-	// 					}
+				let tagName = elm.tagName.toLowerCase()
 
-	// 					let br = document.createElement("br")
-	// 					previousNode.parentNode.insertBefore(br, previousNode.nextSibling)
-
-	// 					previousNode.parentNode.replaceChild(frag, previousNode)
-	// 				} else {
-	// 					previousNode.parentNode.removeChild(previousNode)
-	// 				}
-	// 			}
-				
-	// 			previousNode = n
-	// 		}
-
-	// 		// Final attack
-	// 		if (previousNode) {
-	// 			if (AT.availableTags.includes(previousNode.nodeName.toLowerCase())) {
-	// 				let pn = <HTMLElement> previousNode
-	// 				for (let i = 0; i < pn.attributes.length; i++) {
-	// 					pn.removeAttribute(pn.attributes[i].name)
-	// 				}
-	// 			} else if (previousNode.nodeName.toLowerCase() === "div") {
-	// 				let frag = document.createDocumentFragment()
-	// 				let fc
-	// 				while (fc = previousNode.firstChild) {
-	// 					frag.appendChild(fc)
-	// 				}
-
-	// 				let br = document.createElement("br")
-	// 				previousNode.parentNode.insertBefore(br, previousNode.nextSibling)
-
-	// 				previousNode.parentNode.replaceChild(frag, previousNode)
-	// 			} else {
-	// 				previousNode.parentNode.removeChild(previousNode)
-	// 			}
-	// 		}
-	// 	} else if (phase === 3) {
-	// 		let travelNode = elm.firstChild
-	// 		while (travelNode) {
-	// 			let nextSibling: Node = travelNode.nextSibling
-
-	// 			console.log(travelNode.cloneNode())
-		
-	// 			if (travelNode.nodeName.toLowerCase() === "br") {
-	// 				// br tag
-	// 				if (this.complementP) {
-	// 					elm.insertBefore(this.complementP, travelNode)
-	// 					elm.removeChild(travelNode)
-	// 					this.complementP = undefined
-	// 				} else {
-	// 					elm.removeChild(travelNode)
-	// 				}
-	// 			} else if (AT.topTagsForPaster.includes(travelNode.nodeName.toLowerCase())) {
-	// 				// available tag
-	// 				if (this.complementP) {
-	// 					elm.insertBefore(this.complementP, travelNode)
-	// 					this.complementP = undefined
-	// 				}
-	// 			} else {
-	// 				// unavailable tag
-	// 				if (this.complementP) {
-	// 					this.complementP.appendChild(travelNode)
-	// 				} else {
-	// 					this.complementP = document.createElement("p")
-	// 					this.complementP.appendChild(travelNode)
-	// 				}
-	// 			}
-		
-	// 			;(<Node> travelNode) = nextSibling
-	// 		}
-
-	// 		// Final attack
-	// 		if (this.complementP && this.complementP.firstChild) {
-	// 			elm.appendChild(this.complementP)
-	// 		}
-	// 	}
-	// }
-
-	fixElement(elm: HTMLElement, phase: number) {
-		let allElems = elm.querySelectorAll("*")
-	}
-	loopChildren(elm: HTMLElement, phase: number) {
-		let travelNode = elm.firstChild
-		
-		while (travelNode) {
-			if (phase === 3) {
-				let nextSibling: Node = travelNode.nextSibling
-
-				if (travelNode.nodeName.toLowerCase() === "br") {
-					// br tag
-					if (this.complementP) {
-						elm.insertBefore(this.complementP, travelNode)
-						elm.removeChild(travelNode)
-					} else {
-						elm.removeChild(travelNode)
-					}
-				} else if (AT.topTagsForPaster.includes(travelNode.nodeName.toLowerCase())) {
-					// available tag
-					if (this.complementP) {
-						elm.insertBefore(this.complementP, travelNode)
-					}
+				if (AT.availableTags.includes(tagName)) {
+					console.log(elm)
+					this.removeAllAttributes(elm)
 				} else {
-					// unavailable tag
-					if (this.complementP) {
-						this.complementP.appendChild(travelNode)
-					} else {
-						this.complementP = document.createElement("p")
-						this.complementP.appendChild(travelNode)
-					}
-				}
-
-				;(<Node> travelNode) = nextSibling
-				continue
-			}
-
-			if (travelNode.nodeName.toLowerCase() === "br") {
-				;(<Node> travelNode) = travelNode.nextSibling
-			} else if (travelNode.nodeType === 3) {
-				// TextNode
-				(<Node> travelNode) = travelNode.nextSibling
-				console.log("text Node", travelNode)
-			} else if (TypeChecker.isAvailbaleTag(travelNode.nodeName)) {
-				console.log("Availble tag", travelNode)
-				// Available tag
-				// Remove all attributes
-				let t = <HTMLElement> travelNode
-				console.log(t.attributes)
-				for (let i = 0; i < t.attributes.length; i++) {
-					t.removeAttribute(t.attributes[i].name)
-				}
-				t.removeAttribute("style")
-
-				this.loopChildren(<HTMLElement> travelNode, phase)
-
-				;(<Node> travelNode) = travelNode.nextSibling
-			} else {
-				console.log("unsupported tag", travelNode)
-				let frag, n: Node, isStyledSpanTag = false
-
-				frag = document.createDocumentFragment()
-
-				if (phase === 1) {
-					if (travelNode.nodeName.toLowerCase() === "div") {
-						this.loopChildren(<HTMLElement> travelNode, phase)
-						let br = document.createElement("br")
-						elm.insertBefore(br, travelNode.nextSibling)
-						;(<Node> travelNode) = travelNode.nextSibling
+					if (elm.textContent === "") {
+						elm.parentElement.removeChild(elm)
 						continue
 					}
-				}
 
-				if (travelNode.nodeName.toLowerCase() === "span") {
-					if ((<HTMLElement> travelNode).style.textDecoration === "underline") {
-						frag = document.createElement("u")
-						isStyledSpanTag = true
-					} else if ((<HTMLElement> travelNode).style.textDecoration === "line-through") {
-						frag = document.createElement("strike")
-						isStyledSpanTag = true
+					let displayStyle = this.getDisplayStyle(elm)
+
+					if (displayStyle === "inline" || displayStyle === "inline-block") {
+						let n, frag
+						frag = document.createDocumentFragment()
+
+						if ((<HTMLElement> elm).style.textDecoration) {
+							if ((<HTMLElement> elm).style.textDecoration === "underline") {
+								frag = document.createElement("u")
+							} else if ((<HTMLElement> elm).style.textDecoration === "line-through") {
+								frag = document.createElement("strike")
+							}
+						}
+
+						while (n = elm.firstChild) {
+							frag.appendChild(n)
+						}
+						elm.parentNode.replaceChild(frag, elm)
+					}
+				}
+			}
+
+			fixingTarget.normalize()
+		} else if (phase === 2) {
+			// 모든 div 태그를 제거
+			let allElems = fixingTarget.querySelectorAll("*")
+
+			for (let i = allElems.length - 1; i >= 0; i--) {
+				let n, frag, elm = allElems[i]
+				let tagName = elm.nodeName.toLowerCase()
+
+				if (!AT.availableTags.includes(tagName)) {
+					let childNodes = elm.childNodes
+					let isWrappable = true
+					
+					frag = document.createElement("p")
+					
+					while (n = elm.firstChild) {
+						frag.appendChild(n)
+					}
+					elm.parentNode.replaceChild(frag, elm)
+				}
+			}
+		} else if (phase === 3) {
+			let allElems = fixingTarget.querySelectorAll("p")
+
+			allElems.forEach(elm => {
+				let tagName = elm.nodeName.toLowerCase()
+				let frag, n
+				let childNodes = elm.childNodes
+				let isWrappable = true
+				for (let i = 0; i < childNodes.length; i++) {
+					let displayStyle: string, child = childNodes[i]
+					
+					if (child instanceof Element) {
+						displayStyle = this.getDisplayStyle(child)
+				
+						if (displayStyle !== "inline" && displayStyle !== "inline-block") {
+							isWrappable = false
+							break
+						}
 					}
 				}
 				
-				while (n = travelNode.firstChild) {
-					// Move all children to the document fragment from the unsupported node
-					frag.appendChild(n)
-				}
+				if (!isWrappable) {
+					frag = document.createDocumentFragment()
 
-				let originalTravelNode = travelNode
-				
-				if (isStyledSpanTag) {
-					// Keep traveling from new element's next sibling
-					(<Node> travelNode) = frag.nextSibling
-				} else {
-					// Keep traveling from new first child
-					travelNode = frag.firstChild
+					while (n = elm.firstChild) {
+						frag.appendChild(n)
+					}
+					
+					elm.parentElement.replaceChild(frag, elm)
 				}
-				
-				// Replace the unsupported node with the document fragment
-				elm.replaceChild(frag, originalTravelNode)
+			})
+		} else if (phase === 4) {
+			let childNodes = fixingTarget.childNodes
+
+			for (let i = 0; i < childNodes.length; i++) {
+				let child = childNodes[i]
+				let nodeName = child.nodeName.toLowerCase()
+				if (!AT.topTagsForPaster.includes(nodeName)) {
+					let p = document.createElement("p")
+					child.parentNode.insertBefore(p, child)
+					p.appendChild(child)
+				}
 			}
 		}
+	}
 
-		if (phase === 1) {
-			elm.normalize()
+	getDisplayStyle(elm: Element) {
+		let displayStyle: string
+		if (window.getComputedStyle) {
+			displayStyle = window.getComputedStyle(elm, null).getPropertyValue("display")
+		} else {
+			displayStyle = (<any> elm).currentStyle.display
 		}
+
+		return displayStyle
+	}
+
+	removeAllAttributes(elm: Element) {
+		let atts = []
+		for (let i = 0; i < elm.attributes.length; i++) {
+			atts.push(elm.attributes[i].name)
+		}
+		for (let i = 0; i < atts.length; i++) {
+			if (elm.nodeName.toLowerCase() === "a" && atts[i] === "href") {
+				continue
+			}
+			elm.removeAttribute(atts[i])
+		}
+	}
+
+	paste() {
+		let childNodes = this.pasteArea.childNodes
+		let insertingTarget = EditSession.currentState.node
 	}
 }
