bf1e7927f4312c7e621e27bac76310450fc77a81
Jang Haemin <io@jhaemin.com>
2019-02-27 17:15:12 +0900

    New

---
+++ resources/views/sections/post-view.blade.php
@@ -0,0 +1,6 @@
+<div id="post-view">
+	<div class="pv-backface"></div>
+	<div class="pv-content-container">
+		<h1>Qualcomm’s new 4G, 5G platforms to bring improved telecommunication to vehicles</h1>
+	</div>
+</div>
