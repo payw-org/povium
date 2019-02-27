8a97f86823033f09d73f30761f174b078680c785
Jang Haemin <io@jhaemin.com>
2019-02-27 17:15:00 +0900

    Change copyright text

--- resources/views/sections/globalfooter.blade.php
+++ resources/views/sections/globalfooter.blade.php
@@ -1,4 +1,3 @@
 <footer id="globalfooter">
-	<img src="/assets/images/povium/Povium-logo-shaded.svg" alt="">
-  <span class="copyright"><b style="display: block;">Povium</b><br>Created by Payw &copy; 2019</span>
+  <span class="copyright">Created by Payw &copy; 2019</span>
 </footer>
