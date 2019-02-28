249e47d1362ef5e3a9948dea9201c3eb3b5ae635
Jang Haemin <io@jhaemin.com>
2019-02-28 15:38:44 +0900

    Change structure

--- resources/views/sections/post-view.blade.php
+++ resources/views/sections/post-view.blade.php
@@ -1,6 +1,11 @@
 <div id="post-view">
 	<div class="pv-backface"></div>
 	<div class="pv-content-container">
-		<h1>Qualcomm’s new 4G, 5G platforms to bring improved telecommunication to vehicles</h1>
+		<div class="close-handle"></div>
+		<div class="main">
+			<div class="post-article">
+				<h1 class="title">Qualcomm’s new 4G, 5G platforms to bring improved telecommunication to vehicles</h1>
+			</div>
+		</div>
 	</div>
 </div>
